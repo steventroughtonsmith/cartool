19a143f9dcd8839df038fd155d38daf357f767df
Steven Troughton-Smith <steventroughtonsmith@gmail.com>
2013-07-14 22:12:05 +0100

    Initial Commit

---
+++ cartool/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  cartool
+//
+//  Created by Steven Troughton-Smith on 14/07/2013.
+//  Copyright (c) 2013 High Caffeine Content. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
